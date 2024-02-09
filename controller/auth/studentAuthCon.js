0e2e26f862c818db4b9beb2dbdcc6afd91556c48
Abdallah <abdallah@details.net.sa>
2024-02-10 00:07:39 +0200

    hash the password and populate it from response

--- controller/auth/studentAuthCon.js
+++ controller/auth/studentAuthCon.js
@@ -1,29 +1,57 @@
 import asyncHandler from "express-async-handler";
 import Student from "../../models/StudentModel.js";
 import { validationResult } from "express-validator";
+import bcrypt from "bcrypt";
 
-export const registerStudent = asyncHandler((req, res) => {
+export const registerStudent = asyncHandler(async (req, res) => {
   // Get the validation result
   const errors = validationResult(req);
   // If there are errors, send a 400 response with the error messages
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  } else if (Student.findOne({ email: req.body.email })) {
-    return res
-      .status(500)
-      .json({ errors: [{ msg: `${req.body.email} is already used` }] });
+    return res.status(400).json({ status: false, errors: errors.array() });
+  }
+  let userExist = await Student.findOne({ email: req.body.email });
+  if (userExist) {
+    return res.status(500).json({
+      status: false,
+      errors: [{ msg: `${req.body.email} is already used` }],
+    });
   }
 
+  // hash the password
+  let salt = await bcrypt.genSalt(10);
+  let hashPassword = await bcrypt.hash(req.body.password, salt);
+  let hashPassword2 = await bcrypt.hash(req.body.confirmPassword, salt);
   // If there are no errors, create a new student document with the request body
-  const student = new Student(req.body);
-  student.save();
+  const student = new Student({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashPassword,
+    confirmPassword: hashPassword2,
+    profileImage: req.body.profileImage,
+    age: req.body.age,
+    experience: req.body.experience,
+    track: req.body.track,
+    tracklevel: req.body.tracklevel,
+    militaryStatus: req.body.militaryStatus,
+    about: req.body.about,
+    skills: req.body.skills,
+  });
+  await student.save();
   // Save the student document to the database
-  res.status(201).json({ student });
-  // student.save((err, student) => {
-  //   // If there is an error, send a 500 response with the error message
-  //   if (err) {
-  //     return res.status(500).json({ status: false, error: err.message });
-  //   }
-  //   // If there is no error, send a 201 response with the student data
-  // });
+  res.status(201).json({
+    status: true,
+    user: {
+      username: req.body.username,
+      email: req.body.email,
+      profileImage: req.body.profileImage,
+      age: req.body.age,
+      experience: req.body.experience,
+      track: req.body.track,
+      tracklevel: req.body.tracklevel,
+      militaryStatus: req.body.militaryStatus,
+      about: req.body.about,
+      skills: req.body.skills,
+    },
+  });
 });
