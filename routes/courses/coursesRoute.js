4c75a4381dd8eb05151acdafb86fdc552d9a4b4a
Abdallah <abdallah@details.net.sa>
2024-06-19 16:06:47 +0300

    Courses CRUD

--- routes/courses/coursesRoute.js
+++ routes/courses/coursesRoute.js
@@ -1,13 +1,19 @@
 import express from "express";
 import {
   createCourses,
+  deleteCourse,
   getAllCourses,
+  getCourse,
+  updateCourses,
 } from "../../controller/courses/coursesCon.js";
 import { createCourseValidation } from "../../services/auth/validations.js";
-// import { verifyToken } from "../../middlewares/verifyToken.js";
+import { verifyToken } from "../../middlewares/verifyToken.js";
 const router = express.Router();
 
-router.get("/all", getAllCourses);
-router.get("/create", createCourseValidation, createCourses);
+router.get("/all", verifyToken, getAllCourses);
+router.get("/:id", verifyToken, getCourse);
+router.post("/create", verifyToken, createCourseValidation, createCourses);
+router.put("/update/:id", verifyToken, updateCourses);
+router.delete("/:id", verifyToken, deleteCourse);
 
 export { router as coursesRoute };
